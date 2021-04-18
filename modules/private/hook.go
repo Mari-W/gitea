9cd3cf1722b7e585807ba0a13f6f6cf98b4a1b93
Marius Weidner <marius.weidner@ptwdosimetry.com>
2021-04-18 20:56:20 +0200

    debug

--- modules/private/hook.go
+++ modules/private/hook.go
@@ -233,6 +233,9 @@ func ModifiedFilesList(ownerName string, repoName string, opts HookOptions) (str
 	var revList string
 	var err error
 
+	fmt.Printf("%v", opts.OldCommitIDs)
+	fmt.Printf("%v", opts.NewCommitIDs)
+
 	if opts.OldCommitIDs[0] == "0000000000000000000000000000000000000000" {
 		revList, err = git.NewCommand("rev-list", fmt.Sprintf("%s", opts.NewCommitIDs[0]), "--all").
 			SetDescription(fmt.Sprintf("Reading revs %s", repoName)).
